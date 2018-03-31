3383c257b077a6113d0862e3fe3eac7e1b5ab1e5
bx <bx@space>
2018-03-31 11:11:47 +0300

    Changing polynomials to handle a shift factor, when all coeffs are zeros when less than a given degree

--- primes.hs
+++ primes.hs
@@ -65,35 +65,75 @@ smallestR n = fst . head . filter (\(r, Just k) -> k > floor (logBase 2 (fromInt
 
 eulerTotient n = length [x | x <- [1..n], gcd n x == 1]
 
+-- | Endianness used with polynomials
+data Endianness = Little | Big deriving (Eq, Show)
+
+reverseEndianness Little = Big
+reverseEndianness Big = Little
+
 -- | Little or Big endian polynoms
-data Poly a = LittlePoly [a] | BigPoly [a] deriving (Eq, Show) 
+data Polynomial a = Poly
+  { coeffs      :: [a]
+  , endianness  :: Endianness
+  , shiftFactor :: Integer
+  } deriving (Eq, Show) 
 
-degPoly (LittlePoly ls) = max 0 $ length ls - 1
-degPoly (BigPoly ls) = max 0 $ length ls - 1
+instance Functor Polynomial where
+  fmap f p = p{coeffs = f <$> coeffs p}
 
-reverseendianness (LittlePoly ls) = BigPoly . reverse $ ls
-reverseEndianness (BigPoly ls) = LittlePoly . reverse $ ls
+degPoly p = max 0 $ genericLength (coeffs p) + shiftFactor p - 1
+
+reversePolyEndianness p = p{ coeffs=reverse (coeffs p)
+                           , endianness = reverseEndianness . endianness $ p}
+
+isLittleEndianPoly p = endianness p == Little
+isBigEndianPoly p = endianness p == Big
 
 -- | Most significant coefficient
-mscoeff (LittlePoly []) = 0
-mscoeff (BigPoly []) = 0
-mscoeff (LittlePoly ls) = last ls
-mscoeff (BigPoly ls) = head ls
+msCoeff (Poly [] _ _) = 0
+msCoeff (Poly ls Little _) = last ls
+msCoeff (Poly ls _ _) = head ls
 
-nullPoly = LittlePoly []
+nullPoly = Poly [] Little 0
 
-makeLittleEndian p@(LittlePoly ls) = p
-makeLittleEndian p = reverseEndianness p
+removeSignificantZerosPoly p@(Poly [] _ _) = nullPoly
+removeSignificantZerosPoly p@(Poly (0:ls) Big _) = p{coeffs = ls}
+removeSignificantZerosPoly p
+  | isLittleEndianPoly p && msCoeff p == 0 = removeSignificantZerosPoly p{coeffs = init . coeffs$ p}
+  | otherwise = p
 
-makeBigEndian p@(BigPoly ls) = p
-makeBigEndian p = reverseEndianness p
+removeTrailingZerosPoly p@(Poly [] _ _) = nullPoly
+removeTrailingZerosPoly p@(Poly (0:ls) Little _) = p{coeffs = ls, shiftFactor = 1 + shiftFactor p}
+removeTrailingZerosPoly p
+  | isBigEndianPoly p && (last . coeffs) p == 0 = removeTrailingZerosPoly p{coeffs = init . coeffs $ p, shiftFactor = 1 + shiftFactor p}
+  | otherwise = p
 
-addPoly p1 p2 = LittlePoly (addList lp1 lp2)
+normalizePoly = removeTrailingZerosPoly . removeSignificantZerosPoly
+
+makeLittleEndian p
+  | isLittleEndianPoly p = p
+  | otherwise = reversePolyEndianness p
+
+makeBigEndian p
+  | isBigEndianPoly p = p
+  | otherwise = reversePolyEndianness p
+
+unShiftPoly n p@(Poly _ _ s)
+  | s < 1 || n < 1 = p
+unShiftPoly n p = unShiftPoly (n - 1) unShiftedPoly
   where
-    LittlePoly lp1 = makeLittleEndian p1
-    LittlePoly lp2 = makeLittleEndian p2
+    decShift = shiftFactor p - 1
+    unShiftedPoly
+      | isLittleEndianPoly p = p{coeffs = 0:coeffs p, shiftFactor = decShift}
+      | otherwise = p{coeffs = coeffs p ++ [0], shiftFactor = decShift}
 
-negatePoly p = negate <$> p
+addPoly p p' = Poly (addList (coeffs lp) (coeffs lp')) Little minShift
+  where
+    minShift = min (shiftFactor p) (shiftFactor p')
+    lp = makeLittleEndian . unShiftPoly (shiftFactor p - minShift) $ p
+    lp' = makeLittleEndian . unShiftPoly (shiftFactor p' - minShift) $ p'
+
+negatePoly = fmap negate
 
 addList l [] = l
 addList [] l = l
@@ -101,22 +141,16 @@ addList (l:ls) (l':ls') = (l + l'):addList ls ls'
 
 diffPoly p = addPoly p . negatePoly
 
-extractMSCoeff p@(LittlePoly []) = p
-extractMSCoeff p@(BigPoly []) = p
-extractMSCoeff (LittlePoly ls) = LittlePoly (init ls)
-extractMSCoeff (BigPoly ls) = BigPoly (tail ls)
+extractMSCoeff p@(Poly [] _ _) = p
+extractMSCoeff p
+  | isLittleEndianPoly p = p{coeffs = init . coeffs $ p}
+  | otherwise = p{coeffs = tail . coeffs $ p}
 
 scalePoly scalar = fmap (scalar *)
 
-instance Functor Poly where
-  fmap f (LittlePoly ls) = LittlePoly (fmap f ls)
-  fmap f (BigPoly ls) = BigPoly (fmap f ls)
+increaseDeg  p = p{shiftFactor = 1 + shiftFactor p}
 
-increaseDeg (LittlePoly ls) = LittlePoly (0:ls)
-increaseDeg (BigPoly ls) = BigPoly (ls ++ [0])
-
-increaseByDeg 0 p = p
-increaseByDeg n p = increaseByDeg (n-1) (increaseDeg p)
+increaseByDeg n p = p{shiftFactor = n + shiftFactor p}
 
 polynomialDivision nm dnm
   | degDen <= degNum = (quotient, rest)
@@ -125,8 +159,8 @@ polynomialDivision nm dnm
       degDen = degPoly dnm
       degNum = degPoly nm
       degDiff = degNum - degDen
-      qval = mscoeff nm / mscoeff dnm
-      p = increaseByDeg degDiff $ LittlePoly [qval]
+      qval = msCoeff nm / msCoeff dnm
+      p = increaseByDeg degDiff $ Poly [qval] Little 0
       r = diffPoly (extractMSCoeff nm) . increaseByDeg degDiff . scalePoly qval . extractMSCoeff $ dnm
       (q, rest) = polynomialDivision r dnm
       quotient = addPoly p q
