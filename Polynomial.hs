c2815e0071694c91e3238a88aa52e37743e59742
bx <bx@space>
2018-03-31 12:20:11 +0300

    adding num instance for polynomials, and eval function to replace the variable with a value

--- Polynomial.hs
+++ Polynomial.hs
@@ -18,6 +18,14 @@ data Polynomial a = Poly
 instance Functor Polynomial where
   fmap f p = p{coeffs = f <$> coeffs p}
 
+instance Num a => Num (Polynomial a) where
+  p + p' = addPoly  p p'
+  p * p' = multPoly p p'
+  signum = fmap signum
+  fromInteger n = Poly [fromInteger n] Little 0
+  negate = fmap negate
+  abs = fmap abs
+
 degPoly p = max 0 $ genericLength (coeffs p) + shiftFactor p - 1
 
 reversePolyEndianness p = p{ coeffs=reverse (coeffs p)
@@ -30,6 +38,11 @@ isBigEndianPoly p = endianness p == Big
 msCoeff (Poly [] _ _) = 0
 msCoeff (Poly ls Little _) = last ls
 msCoeff (Poly ls _ _) = head ls
+
+-- | Least significant coefficient
+lsCoeff (Poly [] _ _) = 0
+lsCoeff (Poly ls Big _) = last ls
+lsCoeff (Poly ls _ _) = head ls
 
 nullPoly = Poly [] Little 0
 
@@ -70,17 +83,24 @@ addPoly p p' = Poly (addList (coeffs lp) (coeffs lp')) Little minShift
     lp = makeLittleEndian . unShiftPoly (shiftFactor p - minShift) $ p
     lp' = makeLittleEndian . unShiftPoly (shiftFactor p' - minShift) $ p'
 
-negatePoly = fmap negate
+--negatePoly :: Num a => Polynomial a -> Polynomial a
+-- signature necessary when using: negatePoly = fmap negate
+--negatePoly p = fmap negate p
 
 addList l [] = l
 addList [] l = l
 addList (l:ls) (l':ls') = (l + l'):addList ls ls'
 
-diffPoly p = addPoly p . negatePoly
+diffPoly p = addPoly p . negate
 
 extractMSCoeff p@(Poly [] _ _) = p
 extractMSCoeff p
   | isLittleEndianPoly p = p{coeffs = init . coeffs $ p}
+  | otherwise = p{coeffs = tail . coeffs $ p}
+
+extractLSCoeff p@(Poly [] _ _) = p
+extractLSCoeff p
+  | isBigEndianPoly p = p{coeffs = init . coeffs $ p}
   | otherwise = p{coeffs = tail . coeffs $ p}
 
 scalePoly scalar = fmap (scalar *)
@@ -101,4 +121,14 @@ polynomialDivision nm dnm
       r = diffPoly (extractMSCoeff nm) . increaseByDeg degDiff . scalePoly qval . extractMSCoeff $ dnm
       (q, rest) = polynomialDivision r dnm
       quotient = addPoly p q
-      
+
+evalPoly (Poly [] _ _) _ = 0
+evalPoly p x = (x ^ shiftFactor p) * (lsCoeff p + x * evalPoly (extractLSCoeff p{shiftFactor = 0}) x)
+
+multPoly p p' = res{shiftFactor = shiftFactor p + shiftFactor p'}
+  where
+    sp  = makeLittleEndian $ p {shiftFactor = 0}
+    sp' = makeLittleEndian $ p'{shiftFactor = 0}
+    lstPr [] ls' = []
+    lstPr (l:ls) ls' = addList ((l *) <$> ls') (0:lstPr ls ls')
+    res = Poly (lstPr (coeffs sp) (coeffs sp')) Little 0
